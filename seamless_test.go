5a1109f23be24f7b7bba8a8b6845967b1d05be64
Miki Tebeka <miki.tebeka@gmail.com>
2012-08-10 15:24:24 -0700

    Simple test

---
+++ seamless_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestHttp(t *testing.T) {
+	backend = "hello"
+	port := 6777
+	go startHttpServer(port)
+
+	time.Sleep(1 * time.Second)
+
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/current", port))
+	if err != nil {
+		t.Fatalf("error connecting to /current: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	reply, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("error reading reply: %v\n", err)
+	}
+
+	if string(reply) != fmt.Sprintf("%s\n", backend) {
+		t.Fatalf("bad reply: %s\n", reply)
+	}
+}
